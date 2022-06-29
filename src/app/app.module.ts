a6995d11c97e56ba754dbc241fae58c22fcd7b9e
mk <merlincita@gmail.com>
2022-06-29 14:30:22 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.scss'],
+})
+export class EmployeesComponent implements OnInit {
+  person: FormGroup = this.formBuilder.group({
+    nom: ['', [Validators.required, Validators.minLength(2)]],
+    prenom: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    entreprise: ['', [Validators.required, Validators.minLength(2)]],
+    telephone: ['', [Validators.required, Validators.minLength(10)]],
+  });
+
+  people: any[] = [];
+
+  submitted: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  addPerson(): void {
+    this.people.push(this.person.value);
+    this.person.reset();
+    this.submitted = false;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.person.valid) {
+      this.addPerson();
+    } else {
+      console.log('Formulaire Invalid');
+      console.log(this.person.controls);
+    }
+  }
+
+  whichError(attName: string): string {
+    let temp: string = '';
+    if (this.person.controls[attName].hasError('required')) {
+      temp += '* obligatoire';
+    }
+    if (this.person.controls[attName].hasError('minlength')) {
+      temp += '* minimum characters ';
+      if (attName == 'entreprise') {
+        temp += '10';
+      } else {
+        temp += '2';
+      }
+      //(attName == 'entreprise' ? temp+="10" + temp+="2")
+      // I'm not proud of this workaround
+    }
+    if (this.person.controls[attName].hasError('email')) {
+      temp += '* invalid email address';
+    }
+    return temp;
+  }
+}
